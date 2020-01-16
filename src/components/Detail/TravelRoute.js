e9ca6dd3beb689414fd976ff48134a16fc4f8947
mandaringit <mandarin.dev@kakao.com>
2020-01-16 14:44:43 +0900

    TravelRoute 스타일 변경

--- src/components/Detail/TravelRoute.js
+++ src/components/Detail/TravelRoute.js
@@ -1,43 +1,40 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import styled from 'styled-components';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    width: '100%',
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    fontWeight: theme.typography.fontWeightRegular,
-  },
-}));
+const TravleRouteBlock = styled.div`
+  margin: 1rem;
+  width: '100%';
+`;
+
+const StyledDay = styled.span`
+  font-weight: bold;
+`;
 
 const TravelRoute = ({ routes }) => {
-  const classes = useStyles();
-
   return (
-    <div className={classes.root}>
+    <TravleRouteBlock>
       {routes.map(({ id, imageId, toFrom, description, transport }) => (
-        <ExpansionPanel>
+        <ExpansionPanel key={id}>
           <ExpansionPanelSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography className={classes.heading}>
-              {id}일차, {toFrom}, ({transport})
+            <Typography>
+              ({transport}) <StyledDay>{id}일차</StyledDay> {toFrom}
             </Typography>
           </ExpansionPanelSummary>
           <ExpansionPanelDetails>
-            <Typography>{description}</Typography>
+            <Typography varient="body1">{description}</Typography>
           </ExpansionPanelDetails>
         </ExpansionPanel>
       ))}
-    </div>
+    </TravleRouteBlock>
   );
 };
 
